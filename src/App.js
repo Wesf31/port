7bbeada4a35d22ce919c6eb13b8665aa50c1938b
Wesley Fukui <wes.fukui@gmail.com>
2019-02-09 23:00:20 -0700

    navbar set up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
 import router from './router/router'
 import './App.css';
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar/>
         {router}
       </div>
     );
