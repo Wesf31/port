0c1a692d781d1a2c43fd64f2d84fa34b7baee647
Wesley Fukui <wes.fukui@gmail.com>
2019-02-24 22:32:07 -0700

    set up simple node server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express'),
+  cors = require('cors'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  massive = require('massive')
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+massive(process.env.DB_CONNECTION).then((db) => {
+  app.set('db', db)
+})
+
+app.use(express.static(`${__dirname}/../build`))
+
+app.listen(process.env.SERVER_PORT, () => { console.log(`Server listening on port ${process.env.SERVER_PORT}`) })
