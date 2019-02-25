69fe6a45d3fac16ff83c6e902380cc9942f2e5b6
Wesley Fukui <wes.fukui@gmail.com>
2019-02-24 18:52:50 -0700

    tyring to set up the create route

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Dropzone from 'react-dropzone'
+import { withRouter } from 'react-router-dom'
+
 
 function Create () {
     return (
         <div>
             <p>sup sup</p>
-            <Dropzone>
+            <Dropzone
+                multiple={false}
+                accept="image/*"
+                onDrop={this.onImageDrop}
+                >
                 <p>Drag and drop a picture of some of your past projects or click to select a file.</p>
             </Dropzone>
         </div>
     )
 }
 
-export default Create
+export default withRouter (Create)
