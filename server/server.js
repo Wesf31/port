52b3ea356322b10e4b2663617b49a6321a8adccd
Wesley Fukui <wes.fukui@gmail.com>
2019-03-06 22:43:26 -0700

    simple edit to server

--- server/server.js
+++ server/server.js
@@ -14,6 +14,12 @@ massive(process.env.DB_CONNECTION).then((db) => {
   app.set('db', db)
 })
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}))
+
 app.use(express.static(`${__dirname}/../build`))
 
 app.listen(process.env.SERVER_PORT, () => { console.log(`Server listening on port ${process.env.SERVER_PORT}`) })
