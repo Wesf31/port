c843b68ff8d98a8bfaa3b806294ad62147806035
Wesley Fukui <wes.fukui@gmail.com>
2019-06-05 20:13:22 -0600

    image reducer update

---
+++ src/ducks/imageReducer.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const initialState = {
+  profileData: [],
+  profileDataLoaded: false,
+}
+
+const GET_PROFILE_DATA = 'GET_PROFILE_DATA'
+
+export const setProfileData = profile => ({
+  type: GET_PROFILE_DATA,
+  payload: profile,
+})
+
+export const getProfileData = () => async (dispatch) => {
+  let results = await axios.get('/api/profiles')
+  return dispatch({
+    type: GET_PROFILE_DATA,
+    payload: results.data,
+  })
+}
+
+export default function reducer(state = initialState, action) {
+  const { payload, type } = action
+  switch (type) {
+    case GET_PROFILE_DATA:
+      return { ...state, profileData: payload, profileDataLoaded: true }
+    default:
+      return state
+  }
+}
